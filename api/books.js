bc5235c3b8a718faec82933ab1784c52ae5a9aa0
Vivek Mishra <vivek@gmail.com>
2026-04-04 00:26:47 +0530

    fix: serverless function syntax

--- api/books.js
+++ api/books.js
@@ -1,21 +1,8 @@
 export default function handler(req, res) {
-    // Enable CORS
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-    // Handle preflight
-    if (req.method === 'OPTIONS') {
-        return res.status(200).end();
-    }
-
-    // Return dummy data
-    return res.status(200).json({
-        success: true,
-        message: "API is working!",
+    res.status(200).json({
         books: [
-            { id: 1, title: "The Midnight Library", author: "Matt Haig", price: 399 },
-            { id: 2, title: "Atomic Habits", author: "James Clear", price: 499 }
+            { id: 1, name: "The Midnight Library", price: 399 },
+            { id: 2, name: "Atomic Habits", price: 499 }
         ]
     });
 }
